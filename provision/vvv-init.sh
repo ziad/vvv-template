741ee617a8b2edca576391ba1ab130e678cec8a9
Ziad Hakim <ziad.hakim@holosbase.com>
2017-12-28 13:09:41 +0100

    add files

---
+++ provision/vvv-init.sh
@@ -0,0 +1,7 @@
+# Nginx Logs
+mkdir -p ${VVV_PATH_TO_SITE}/log
+touch ${VVV_PATH_TO_SITE}/log/error.log
+touch ${VVV_PATH_TO_SITE}/log/access.log
+
+# public_html
+mkdir -p ${VVV_PATH_TO_SITE}/public_html
